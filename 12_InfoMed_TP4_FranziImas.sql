255af05801e79b46d8896bec6c47be0a05ffd0ab
justina214 <120291001+justina214@users.noreply.github.com>
2025-05-26 04:01:56 +0200

    Update 12_InfoMed_TP4_FranziImas.sql

--- 12_InfoMed_TP4_FranziImas.sql
+++ 12_InfoMed_TP4_FranziImas.sql
@@ -1,6 +1,15 @@
-SELECT med.nombre AS nombre_medicamento, COUNT(*) AS veces_recetado
+–--vemos todas las recetas del medico
+SELECT med.nombre AS nombre_medico,dro.nombre AS remedio
 FROM recetas r
-JOIN medicamentos med ON r.id_medicamento=med.id_medicamento
-GROUP BY med.nombre
-ORDER BY veces_recetado DESC
-LIMIT 1;
+JOIN medicos med ON r.id_medico=med.id_medico
+JOIN medicamentos dro ON r.id_medicamento=dro.id_medicamento
+WHERE r.id_medico=2;
+–--seleccionamos las que se repetian 2 o mas veces
+SELECT med.nombre AS nombre_medico, dro.nombre AS remedio, COUNT(*) AS veces_prescrito
+FROM recetas r
+JOIN medicos med ON r.id_medico=med.id_medico
+JOIN medicamentos dro ON r.id_medicamento=dro.id_medicamento
+WHERE r.id_medico=2
+GROUP BY med.nombre, dro.nombre
+HAVING COUNT(*)>1
+ORDER BY veces_prescrito DESC;
