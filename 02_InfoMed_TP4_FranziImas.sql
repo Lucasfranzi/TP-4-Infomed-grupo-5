1957b7244e98543c867dbe2469f9b6b436a32e3f
justina214 <120291001+justina214@users.noreply.github.com>
2025-05-26 03:52:16 +0200

    Update 02_InfoMed_TP4_FranziImas.sql

--- 02_InfoMed_TP4_FranziImas.sql
+++ 02_InfoMed_TP4_FranziImas.sql
@@ -1,4 +1,4 @@
 CREATE VIEW vista_pacientes AS
-SELECT id_oaciente, nombre, fecha_nacimiento,
+SELECT id_paciente, nombre, fecha_nacimiento,
 	EXTRACT(YEAR FROM AGE(fecha_nacimiento)) AS edad
 FROM pacientes;
