2f7d0359d3539a4f0bb7532e305861598bd6d38f
justina214 <120291001+justina214@users.noreply.github.com>
2025-05-27 02:10:14 +0200

    Update 05_InfoMed_TP4_FranziImas.sql

--- 05_InfoMed_TP4_FranziImas.sql
+++ 05_InfoMed_TP4_FranziImas.sql
@@ -2,20 +2,20 @@
 ---SELECT DISTINCT ciudad FROM pacientes;
 ---busco reducir las diferencias entre las opciones
 UPDATE pacientes
-SET ciudad =TRIM(INITCAP(ciudad));
-SELECT DISTINCT ciudad FROM pacientes;
+SET ciudad = TRIM(INITCAP(ciudad));
 ---la siguiente forma es bastante ineficiente para solucionar este problema
 ---solo se puede hacer porque es una base chica
 UPDATE pacientes
 SET ciudad = 'BUENOS AIRES'
-WHERE ciudad IN ('buenos aires','Bs Aires', 'Buenos Aires', 'Buenos aires', 'buenos Aires', 'Buenos    Aires');
+WHERE ciudad IN ('buenos aires','Bs Aires','Buenos aires','Buenos Aires',
+                 'buenos Aiers','Buenos    Aires', 'Buenos   Aires', 'Buenos Aiers');
 UPDATE pacientes
-SET ciudad= 'CORDOBA'
-WHERE ciudad IN ('Córodba','Cordoba');
+SET ciudad = 'CORDOBA'
+WHERE ciudad IN ('Córdoba', 'Cordoba');
 UPDATE pacientes
-SET ciudad='SANTA FE'
+SET ciudad = 'SANTA FE'
 WHERE ciudad IN ('Santa Fe');
 UPDATE pacientes
-SET ciudad ='MENDOZA'
-where CIUDAD in ('Menzoa', 'Mendoza');
+SET ciudad = 'MENDOZA'
+WHERE ciudad IN ('Mendzoa',' Mendoza');
 SELECT DISTINCT ciudad FROM pacientes;
